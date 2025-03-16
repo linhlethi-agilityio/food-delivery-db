a61a60f7255c973ef29c68c37edbbe9c5a8a4e28
Linh Thi Le <linh.lethi@asnet.com.vn>
2025-03-16 22:40:46 +0700

    feat: update auth

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const jsonServer = require('json-server');
+const auth = require('json-server-auth');
+
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
@@ -6,5 +8,6 @@ const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
+server.use(auth);
 
 server.listen(port);
