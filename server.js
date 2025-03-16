2f23f71551d6b69aabc7025e93e28905687c274b
Linh Thi Le <linh.lethi@asnet.com.vn>
2025-03-16 22:51:20 +0700

    fix database

--- server.js
+++ server.js
@@ -1,15 +1,37 @@
 const jsonServer = require('json-server');
-const auth = require('json-server-auth');
+const jsonServerAuth = require('json-server-auth');
+
+// Create a JSON Server instance
 const server = jsonServer.create();
+
+// Create a router using the 'db.json' file as the data source
 const router = jsonServer.router('db.json');
+
+// Set up default middlewares (logger, static, cors, etc.)
 const middlewares = jsonServer.defaults();
 
-server.db = router.db; // Needed for `json-server-auth`
-
+// Use the middlewares
 server.use(middlewares);
-server.use(auth);
+
+// Share the database of the router with jsonServerAuth
+server.db = router.db;
+
+const rules = jsonServerAuth.rewriter({
+  // Permission rules
+  users: 640,
+});
+
+// You must apply the middlewares in the following order
+server.use(rules);
+
+// Use jsonServerAuth for authentication and user management
+server.use(jsonServerAuth);
+
+// Use the router for handling API routes
 server.use(router);
 
-server.listen(3000, () => {
-  console.log('JSON Server is running on http://localhost:3000');
+// Start the server on port 3001
+server.listen(3001, () => {
+  // eslint-disable-next-line no-console
+  console.log('JSON Server is running port 3001');
 });
